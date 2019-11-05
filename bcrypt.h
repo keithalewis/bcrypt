dca9f42e4542b7d317ea77700832cbe41cb866dc
kal <kal@kalx.net>
2019-11-05 17:27:56 -0500

    sync

--- bcrypt.h
+++ bcrypt.h
@@ -11,8 +11,10 @@ namespace win {
 			BCRYPT_ALG_HANDLE hAlgorithm;
 			DWORD cbKeyObject;
 			DWORD cbData;
+			PBYTE pbKeyObject;
 		public:
 			OpenAlgorithmProvider(LPCWSTR pszAlgid = BCRYPT_AES_ALGORITHM, LPCWSTR pszImplementation = NULL, ULONG dwFlags = 0)
+				: hAlgorithm(0), cbKeyObject(0), cbData(0), pbKeyObject(0)
 			{
 				NTSTATUS status;
 
@@ -20,6 +22,8 @@ namespace win {
 				//!! check status
 				status = BCryptGetProperty(hAlgorithm, BCRYPT_OBJECT_LENGTH, 
 					(PBYTE)&cbKeyObject, sizeof(DWORD), &cbData, 0);
+
+				status = status;
 			}
 		};
 	}
